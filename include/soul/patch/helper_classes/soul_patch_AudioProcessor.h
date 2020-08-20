393f0b039456ebf3eb4103efde7050557e84cb82
jules <julianstorer@gmail.com>
2020-08-20 09:59:36 +0100

    Added a custom value-change callback to SOULPatchAudioProcessor::PatchParameter

--- include/soul/patch/helper_classes/soul_patch_AudioProcessor.h
+++ include/soul/patch/helper_classes/soul_patch_AudioProcessor.h
@@ -175,7 +175,8 @@ struct SOULPatchAudioProcessor    : public juce::AudioPluginInstance,
         if (player != nullptr)
             for (auto& p : player->getParameters())
                 if (! getFlagState (*p, "hidden", false))
-                    treeBuilder.addParam (std::make_unique<PatchParameter> (p), String::Ptr (p->getProperty ("group")));
+                    treeBuilder.addParam (std::make_unique<PatchParameter> (p),
+                                          String::Ptr (p->getProperty ("group")));
 
         setParameterTree (std::move (treeBuilder.tree));
     }
@@ -482,6 +483,211 @@ struct SOULPatchAudioProcessor    : public juce::AudioPluginInstance,
     // This is public to allow custom GUIs to interact with it, but should be used with caution!
     juce::MidiKeyboardState midiKeyboardState;
 
+    //==============================================================================
+    struct PatchParameter  : public juce::AudioProcessorParameterWithID
+    {
+        PatchParameter (soul::patch::Parameter::Ptr p)
+            : AudioProcessorParameterWithID (p->ID, p->name),
+              param (std::move (p)),
+              unit (param->unit.toString<juce::String>()),
+              textValues (parseTextValues (String::Ptr (param->getProperty ("text")))),
+              range (param->minValue, param->maxValue, param->step),
+              initialValue (param->initialValue),
+              numDecimalPlaces (getNumDecimalPlaces (range)),
+              isBool (getFlagState (*param, "boolean", false)),
+              automatable (getFlagState (*param, "automatable", true))
+        {
+        }
+
+        const soul::patch::Parameter::Ptr param;
+        const juce::String unit;
+        const juce::StringArray textValues;
+        const juce::NormalisableRange<float> range;
+        const float initialValue;
+        const int numDecimalPlaces;
+        const bool isBool, automatable;
+
+        std::function<void(float)> valueChangedCallback;
+
+        juce::String getName (int maximumStringLength) const override    { return name.substring (0, maximumStringLength); }
+        juce::String getLabel() const override                           { return unit; }
+        Category getCategory() const override                            { return genericParameter; }
+        bool isDiscrete() const override                                 { return range.interval != 0; }
+        bool isBoolean() const override                                  { return isBool; }
+        bool isAutomatable() const override                              { return automatable; }
+        bool isMetaParameter() const override                            { return false; }
+        juce::StringArray getAllValueStrings() const override            { return textValues; }
+
+        float getDefaultValue() const override                           { return convertTo0to1 (initialValue); }
+        float getValue() const override                                  { return convertTo0to1 (param->getValue()); }
+
+        void setValue (float newValue) override
+        {
+            auto fullRange = convertFrom0to1 (newValue);
+
+            if (fullRange != param->getValue())
+            {
+                param->setValue (fullRange);
+
+                if (valueChangedCallback != nullptr)
+                    valueChangedCallback (fullRange);
+
+                sendValueChangedMessageToListeners (newValue);
+            }
+        }
+
+        juce::String getText (float v, int length) const override
+        {
+            juce::String result;
+
+            if (textValues.isEmpty())
+                result = juce::String (convertFrom0to1 (v), numDecimalPlaces);
+            else if (textValues.size() == 1)
+                result = preprocessText (textValues[0].toUTF8(), convertFrom0to1 (v));
+            else
+                result = textValues[juce::jlimit (0, textValues.size() - 1, juce::roundToInt (v * (textValues.size() - 1.0f)))];
+
+            return length > 0 ? result.substring (0, length) : result;
+        }
+
+        float getValueForText (const juce::String& text) const override
+        {
+            for (int i = 0; i < textValues.size(); ++i)
+                if (textValues[i] == text)
+                    return i / (textValues.size() - 1.0f);
+
+            return convertTo0to1 (text.upToLastOccurrenceOf (text, false, false).getFloatValue());
+        }
+
+        int getNumSteps() const override
+        {
+            if (! textValues.isEmpty() && std::abs (textValues.size() - (range.end - range.start)) < 0.01f)
+                return textValues.size() - 1;
+
+            if (range.interval > 0)
+                return static_cast<int> ((range.end - range.start) / range.interval) + 1;
+
+            return AudioProcessor::getDefaultNumParameterSteps();
+        }
+
+    private:
+        float convertTo0to1 (float v) const    { return range.convertTo0to1 (range.snapToLegalValue (v)); }
+        float convertFrom0to1 (float v) const  { return range.snapToLegalValue (range.convertFrom0to1 (juce::jlimit (0.0f, 1.0f, v))); }
+
+        static int getNumDecimalPlaces (juce::NormalisableRange<float> r)
+        {
+            int places = 7;
+
+            if (r.interval != 0.0f)
+            {
+                if (juce::approximatelyEqual (std::abs (r.interval - (int) r.interval), 0.0f))
+                    return 0;
+
+                auto v = std::abs (juce::roundToInt (r.interval * pow (10, places)));
+
+                while ((v % 10) == 0 && places > 0)
+                {
+                    --places;
+                    v /= 10;
+                }
+            }
+
+            return places;
+        }
+
+        static juce::StringArray parseTextValues (String::Ptr text)
+        {
+            if (text != nullptr)
+                return juce::StringArray::fromTokens (text.toString<juce::String>().unquoted(), "|", {});
+
+            return {};
+        }
+
+        static juce::String preprocessText (juce::CharPointer_UTF8 text, float value)
+        {
+            juce::MemoryOutputStream result;
+
+            while (! text.isEmpty())
+            {
+                auto c = text.getAndAdvance();
+
+                if (c != '%')
+                {
+                    result << juce::String::charToString (c);
+                    continue;
+                }
+
+                auto format = text;
+                bool addSignChar = false;
+
+                if (*format == '+')
+                {
+                    addSignChar = true;
+                    ++format;
+                }
+
+                bool isPadded = (*format == '0');
+                int numDigits = 0;
+
+                while (format.isDigit())
+                    numDigits = numDigits * 10 + (format.getAndAdvance() - '0');
+
+                bool isFloat = (*format == 'f');
+                bool isInt   = (*format == 'd');
+
+                if (! (isInt || isFloat))
+                {
+                    result << '%';
+                    continue;
+                }
+
+                if (addSignChar && value >= 0)
+                    result << '+';
+
+                if (isInt)
+                {
+                    juce::String s ((int64_t) (value + 0.5f));
+                    result << (isPadded ? s.paddedLeft ('0', numDigits) : s);
+                }
+                else if (numDigits <= 0)
+                {
+                    result << value;
+                }
+                else if (isPadded)
+                {
+                    result << juce::String (value, numDigits);
+                }
+                else
+                {
+                    juce::String s (value);
+                    auto afterDot = s.fromLastOccurrenceOf (".", false, false);
+
+                    if (afterDot.containsOnly ("0123456789"))
+                        if (afterDot.length() > numDigits)
+                            s = s.dropLastCharacters (afterDot.length() - numDigits);
+
+                    result << s;
+                }
+
+                text = ++format;
+            }
+
+            return result.toString();
+        }
+    };
+
+    //==============================================================================
+    std::vector<PatchParameter*> getPatchParameters() const
+    {
+        std::vector<PatchParameter*> params;
+        params.reserve (static_cast<size_t> (getParameters().size()));
+
+        for (auto* p : getParameters())
+            params.push_back (dynamic_cast<PatchParameter*> (p));
+
+        return params;
+    }
+
 private:
     //==============================================================================
     soul::patch::PatchInstance::Ptr patch;
@@ -630,193 +836,6 @@ private:
             }
         }
     }
-
-    //==============================================================================
-    struct PatchParameter  : public juce::AudioProcessorParameterWithID
-    {
-        PatchParameter (soul::patch::Parameter::Ptr p)
-            : AudioProcessorParameterWithID (p->ID, p->name),
-              param (std::move (p)),
-              unit (param->unit.toString<juce::String>()),
-              textValues (parseTextValues (String::Ptr (param->getProperty ("text")))),
-              range (param->minValue, param->maxValue, param->step),
-              initialValue (param->initialValue),
-              numDecimalPlaces (getNumDecimalPlaces (range)),
-              isBool (getFlagState (*param, "boolean", false)),
-              automatable (getFlagState (*param, "automatable", true))
-        {
-        }
-
-        const soul::patch::Parameter::Ptr param;
-        const juce::String unit;
-        const juce::StringArray textValues;
-        const juce::NormalisableRange<float> range;
-        const float initialValue;
-        const int numDecimalPlaces;
-        const bool isBool, automatable;
-
-        juce::String getName (int maximumStringLength) const override    { return name.substring (0, maximumStringLength); }
-        juce::String getLabel() const override                           { return unit; }
-        Category getCategory() const override                            { return genericParameter; }
-        bool isDiscrete() const override                                 { return range.interval != 0; }
-        bool isBoolean() const override                                  { return isBool; }
-        bool isAutomatable() const override                              { return automatable; }
-        bool isMetaParameter() const override                            { return false; }
-        juce::StringArray getAllValueStrings() const override            { return textValues; }
-
-        float getDefaultValue() const override                           { return convertTo0to1 (initialValue); }
-        float getValue() const override                                  { return convertTo0to1 (param->getValue()); }
-
-        void setValue (float newValue) override
-        {
-            auto fullRange = convertFrom0to1 (newValue);
-
-            if (fullRange != param->getValue())
-            {
-                param->setValue (fullRange);
-                sendValueChangedMessageToListeners (newValue);
-            }
-        }
-
-        juce::String getText (float v, int length) const override
-        {
-            juce::String result;
-
-            if (textValues.isEmpty())
-                result = juce::String (convertFrom0to1 (v), numDecimalPlaces);
-            else if (textValues.size() == 1)
-                result = preprocessText (textValues[0].toUTF8(), convertFrom0to1 (v));
-            else
-                result = textValues[juce::jlimit (0, textValues.size() - 1, juce::roundToInt (v * (textValues.size() - 1.0f)))];
-
-            return length > 0 ? result.substring (0, length) : result;
-        }
-
-        float getValueForText (const juce::String& text) const override
-        {
-            for (int i = 0; i < textValues.size(); ++i)
-                if (textValues[i] == text)
-                    return i / (textValues.size() - 1.0f);
-
-            return convertTo0to1 (text.upToLastOccurrenceOf (text, false, false).getFloatValue());
-        }
-
-        int getNumSteps() const override
-        {
-            if (! textValues.isEmpty() && std::abs (textValues.size() - (range.end - range.start)) < 0.01f)
-                return textValues.size() - 1;
-
-            if (range.interval > 0)
-                return static_cast<int> ((range.end - range.start) / range.interval) + 1;
-
-            return AudioProcessor::getDefaultNumParameterSteps();
-        }
-
-    private:
-        float convertTo0to1 (float v) const    { return range.convertTo0to1 (range.snapToLegalValue (v)); }
-        float convertFrom0to1 (float v) const  { return range.snapToLegalValue (range.convertFrom0to1 (juce::jlimit (0.0f, 1.0f, v))); }
-
-        static int getNumDecimalPlaces (juce::NormalisableRange<float> r)
-        {
-            int places = 7;
-
-            if (r.interval != 0.0f)
-            {
-                if (juce::approximatelyEqual (std::abs (r.interval - (int) r.interval), 0.0f))
-                    return 0;
-
-                auto v = std::abs (juce::roundToInt (r.interval * pow (10, places)));
-
-                while ((v % 10) == 0 && places > 0)
-                {
-                    --places;
-                    v /= 10;
-                }
-            }
-
-            return places;
-        }
-
-        static juce::StringArray parseTextValues (String::Ptr text)
-        {
-            if (text != nullptr)
-                return juce::StringArray::fromTokens (text.toString<juce::String>().unquoted(), "|", {});
-
-            return {};
-        }
-
-        static juce::String preprocessText (juce::CharPointer_UTF8 text, float value)
-        {
-            juce::MemoryOutputStream result;
-
-            while (! text.isEmpty())
-            {
-                auto c = text.getAndAdvance();
-
-                if (c != '%')
-                {
-                    result << juce::String::charToString (c);
-                    continue;
-                }
-
-                auto format = text;
-                bool addSignChar = false;
-
-                if (*format == '+')
-                {
-                    addSignChar = true;
-                    ++format;
-                }
-
-                bool isPadded = (*format == '0');
-                int numDigits = 0;
-
-                while (format.isDigit())
-                    numDigits = numDigits * 10 + (format.getAndAdvance() - '0');
-
-                bool isFloat = (*format == 'f');
-                bool isInt   = (*format == 'd');
-
-                if (! (isInt || isFloat))
-                {
-                    result << '%';
-                    continue;
-                }
-
-                if (addSignChar && value >= 0)
-                    result << '+';
-
-                if (isInt)
-                {
-                    juce::String s ((int64_t) (value + 0.5f));
-                    result << (isPadded ? s.paddedLeft ('0', numDigits) : s);
-                }
-                else if (numDigits <= 0)
-                {
-                    result << value;
-                }
-                else if (isPadded)
-                {
-                    result << juce::String (value, numDigits);
-                }
-                else
-                {
-                    juce::String s (value);
-                    auto afterDot = s.fromLastOccurrenceOf (".", false, false);
-
-                    if (afterDot.containsOnly ("0123456789"))
-                        if (afterDot.length() > numDigits)
-                            s = s.dropLastCharacters (afterDot.length() - numDigits);
-
-                    result << s;
-                }
-
-                text = ++format;
-            }
-
-            return result.toString();
-        }
-    };
 
     //==============================================================================
     struct ParameterTreeGroupBuilder
