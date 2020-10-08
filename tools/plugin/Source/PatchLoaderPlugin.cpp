18a97d55348282e249c2c48d058bbfbef9a1c2ae
jules <julianstorer@gmail.com>
2020-10-08 19:30:20 +0100

    Tidied up a comment

--- tools/plugin/Source/PatchLoaderPlugin.cpp
+++ tools/plugin/Source/PatchLoaderPlugin.cpp
@@ -1,17 +1,22 @@
 /*
-  ==============================================================================
+     _____ _____ _____ __
+    |   __|     |  |  |  |
+    |__   |  |  |  |  |  |__
+    |_____|_____|_____|_____|
 
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
+    Copyright (c) 2018 - ROLI Ltd.
 */
 
 #include <JuceHeader.h>
 #include "../../../include/soul/patch/helper_classes/soul_patch_LoaderPlugin.h"
 
 
-
 //==============================================================================
+/**
+    All the classes that do the actual work here are header-only classes in
+    the soul include folder, so this is all we need to do here is include them
+    and use them to instantiate a plugin...
+*/
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new soul::patch::SOULPatchLoaderPlugin<soul::patch::PatchLibraryDLL> ({});
